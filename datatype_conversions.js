5e4bcc6b184bf8151daf1845e3c8e9425f57cd52
Kunal Thakur <kunalx2006@gmail.com>
2026-01-11 19:59:09 +0530

    data_typeconversion

---
+++ datatype_conversions.js
@@ -0,0 +1,16 @@
+let score = "32";
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+score = "33abc";
+valueInNumber = Number(score); // not pure number
+console.log(valueInNumber);
+// in numer:-
+// "33"=> 33, "33abc"=> NaN, true=> 1, null=> 0, undefined=NaN
+
+// in boolean
+// 1=> true, ""=> false, "kunal"=> true
+
+// in string
+// 33=>"33",
